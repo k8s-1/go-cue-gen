0d7025092b7b8459ba8d2bd6f47b8a20fa8c7580
pman <pman-$-432894328923@gmail.com>
2024-10-30 14:38:25 +0100

    ...

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"cuelang.org/go/cue/cuecontext"
 	"cuelang.org/go/cue/load"
+
+	"cuelang.org/go/encoding/yaml"
 )
 
 func main() {
@@ -12,4 +15,11 @@ func main() {
 	insts := load.Instances([]string{"."}, nil)
 	v := ctx.BuildInstance(insts[0])
 	fmt.Printf("%v\n", v)
+
+	b, err := yaml.Encode(v)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+  fmt.Printf("%s\n", string(b))
 }
